39e786c3f533d3b0582f41def770e6ecc44d8666
Kiran Holi <kiran.holi@senecaglobal.com>
2020-08-29 20:22:18 +0530

    Added MVC project

---
+++ MvcVehicleStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcVehicleStore.Data;
+
+[assembly: HostingStartup(typeof(MvcVehicleStore.Areas.Identity.IdentityHostingStartup))]
+namespace MvcVehicleStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
